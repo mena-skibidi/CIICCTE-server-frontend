4a741ea901de8582ea0f3a519209bf37305df6b0
Adam Mena <adam.menq@icloud.com>
2026-08-18 12:09:59 -0600

    vite repo setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+    <div className="bg-white w-dvw h-dvh flex justify-center items-center">
+      <div className="w-1/4 h-1/2 border-1 rounded-2xl">
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
